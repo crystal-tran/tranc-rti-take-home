81074a6d37141f1777ad90fcd8ad3388be779bc2
Crystal Tran <c.t.tran95@gmail.com>
2024-07-11 20:29:55 -0400

    Updated docstrings

--- WaterJugSystem.ts
+++ WaterJugSystem.ts
@@ -1,9 +1,10 @@
-type Jug = {
+interface Jug {
   name: string;
   capacity: number;
   currentVolume: number;
 }
 
+/**Creates a water jug */
 function createWaterJug(name: string, capacity: number) : Jug {
   return {
     name,
@@ -12,17 +13,22 @@ function createWaterJug(name: string, capacity: number) : Jug {
   };
 }
 
-function fill(jug: Jug) {
+
+  /**Fills jug to its maximum capacity*/
+function fill(jug: Jug) : void {
   jug.currentVolume = jug.capacity;
   console.log(`Filled jug ${jug.name} to ${jug.capacity}`);
 }
 
-function spill(jug: Jug) {
+/**Empties jug*/
+function spill(jug: Jug) : void {
   jug.currentVolume = 0;
   console.log(`Spilled jug ${jug.name}`);
 }
 
-function pour(sourceJug : Jug, targetJug: Jug) {
+
+  /**Pours water from source jug into target jug*/
+function pour(sourceJug : Jug, targetJug: Jug) : void {
   const pourAmount = Math.min(
     sourceJug.currentVolume,
     targetJug.capacity - targetJug.currentVolume
@@ -32,17 +38,28 @@ function pour(sourceJug : Jug, targetJug: Jug) {
   console.log(`Poured ${pourAmount} from jug ${sourceJug.name} to jug ${targetJug.name}`);
 }
 
+
+/**Performs a series of actions to achieve target capacity in target jug.
+   *
+   * Process:
+   * 1) If source jug is empty, fill source jug
+   * 2) If there's water in source jug and target jug is not at target capacity,
+   *    pour from source jug to target jug
+   * 3) If target jug is full, spill target jug
+   *
+   * Returns a string when target capacity is achieved.
+*/
 function reachGoal(sourceJug : Jug, targetJug : Jug, targetCapacity : number) {
-  while (jugB.currentVolume !== targetCapacity) {
-    if (jugA.currentVolume === 0) {
-      fill(jugA);
-    } else if (jugA.currentVolume > 0 && jugB.currentVolume < jugB.capacity) {
-      pour(jugA, jugB);
-    } else if (jugB.currentVolume === jugB.capacity) {
-      spill(jugB);
+  while (targetJug.currentVolume !== targetCapacity) {
+    if (sourceJug.currentVolume === 0) {
+      fill(sourceJug);
+    } else if (sourceJug.currentVolume > 0 && targetJug.currentVolume < targetJug.capacity) {
+      pour(sourceJug, targetJug);
+    } else if (targetJug.currentVolume === targetJug.capacity) {
+      spill(targetJug);
     }
   }
-  return `Goal reached! Jug ${jugB.name} contains ${jugB.currentVolume} gallons.`;
+  return `Goal reached! Jug ${targetJug.name} contains ${targetJug.currentVolume} gallons.`;
 }
 
 // Example usage:
